f4818f57637e113576c422b8504f0c3e8f21c91e
Omar Zuniga <miguel_omar30@hotmail.com>
2025-11-01 19:03:53 -0600

    Hero Section

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+import { HeroHeader } from "@/app/(marketing)/_components/header";
+import { HeroSection } from "@/app/(marketing)/_components/hero-section";
+
+const Home = () => {
+	return (
+		<div>
+			<HeroHeader />
+			<HeroSection />
+		</div>
+	);
+};
+
+export default Home;
